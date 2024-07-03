642a21e0563e9096042bb1f500d4df870f57d608
Bhavjot Singh <109903185+thebhavjotsingh@users.noreply.github.com>
2024-07-03 16:20:45 -0600

    display test modified

--- display_test.py
+++ display_test.py
@@ -17,10 +17,10 @@ for row in range(no_rows):
         red_count = 0
         for i in range(len(row_cards)):
             if row_cards[i].get_suit() in ['hearts','diamonds']:
-                row_line += f"\033[31m{str(row_cards[i]).split('\n')[line]}\033[00m"
+                row_line += f"\033[31m{repr(row_cards[i]).split('\n')[line]}\033[00m"
                 red_count += 10 # each colored card needs ten more spaces for proper indentation
             else: 
-                row_line += str(row_cards[i]).split('\n')[line]
+                row_line += repr(row_cards[i]).split('\n')[line]
             if i != 3:
                 row_line += "  "
         row_line = f"{row_line:^{78+red_count}}"
